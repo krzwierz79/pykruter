6d686e30be1c128c2c60adfe2dfee8685ed44b53
krzwierz79 <krzwierz79@gmail.com>
2022-03-31 23:29:29 +0200

    extract keywords

--- data/questions/analysis/keywords.py
+++ data/questions/analysis/keywords.py
@@ -1,14 +1,21 @@
 # https://pypi.org/project/rake-nltk/
 from rake_nltk import Rake
 # get all text
+with open('pykruter_data.csv', encoding='utf-8') as f:
+    q_and_a = f.read()
 
 # Uses stopwords for english from NLTK, and all punctuation characters by
 # default
 # r = Rake()
-r = Rake(max_length=1)  # może 2 do kolokacji
+r = Rake(min_length=1, max_length=2)  # może 2 do kolokacji
 
 # Extraction given the text.
 r.extract_keywords_from_text(q_and_a)
+ranked = r.get_ranked_phrases()
+print(len(ranked))
+print(ranked[:10])
+by_dict = list(dict.fromkeys(ranked))
+
 
 # add human-generated keywords
 
