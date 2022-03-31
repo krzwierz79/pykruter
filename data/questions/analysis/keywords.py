45519f689f628b228e3b669da6afb5911ad3db6d
krzwierz79 <krzwierz79@gmail.com>
2022-03-31 23:43:15 +0200

    length tweaks

--- data/questions/analysis/keywords.py
+++ data/questions/analysis/keywords.py
@@ -1,19 +1,21 @@
 # https://pypi.org/project/rake-nltk/
 from rake_nltk import Rake
 # get all text
-with open('pykruter_data.csv', encoding='utf-8') as f:
+with open('data.txt', encoding='utf-8') as f:
     q_and_a = f.read()
 
 # Uses stopwords for english from NLTK, and all punctuation characters by
 # default
+
 # r = Rake()
-r = Rake(min_length=1, max_length=2)  # może 2 do kolokacji
+# może 2 do kolokacji
+r = Rake(min_length=2, max_length=3, include_repeated_phrases=False)
 
 # Extraction given the text.
 r.extract_keywords_from_text(q_and_a)
 ranked = r.get_ranked_phrases()
 print(len(ranked))
-print(ranked[:10])
+print(ranked[:100])
 by_dict = list(dict.fromkeys(ranked))
 
 
