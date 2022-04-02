6a3eda798a64db00d7029a7f9dce97b2a809daf9
krzwierz79 <krzwierz79@gmail.com>
2022-04-02 18:25:59 +0200

    next scrape and regex search

--- data/questions/03_txt_xlsx.py
+++ data/questions/03_txt_xlsx.py
@@ -1,18 +1,43 @@
 import pyexcel as p
-qa_collection = []
-
-with open('hackr_io.txt', encoding="utf-8") as file:
-    listed = [line for line in file.readlines()]
-    # print(listed[:20])
+import re
 
 
-# make new list elements for lines with "Q." append other lines
-for ln_num, line in enumerate(listed):
-    # if "?" in line:  # if "?" and "."
-    if "Question:" in line:
-        qa_collection.append([line, ""])
-    else:
-        qa_collection[-1][1] += line
+def open_txt(filename):
+    with open(f"{filename}.txt", encoding="utf-8") as file:
+        listed = [line for line in file.readlines()]
+        # print(listed[:20])
+    return [listed, filename]
 
-print(qa_collection[:4])
-p.save_as(array=qa_collection, dest_file_name="hackr_io.xls")
+
+def easy_find(what_where):
+    qa_collection = []
+    listed, filename = what_where
+    # make new list elements for lines with "Q." append other lines
+    for ln_num, line in enumerate(listed):
+        # if "?" in line:  # if "?" and "."
+        if "?" in line:
+            qa_collection.append([line, ""])
+        else:
+            qa_collection[-1][1] += line
+
+    # print(qa_collection[:4])
+    p.save_as(array=qa_collection, dest_file_name=f"{filename}.xls")
+
+
+def regex_find(what_where):
+    qa_collection = []
+    pattern = re.compile('^\d{1,2}\)', re.MULTILINE)
+    listed, filename = what_where
+    # make new list elements for lines with "Q." append other lines
+    for ln_num, line in enumerate(listed):
+        if re.match(pattern, line):
+            qa_collection.append([line, ""])
+        else:
+            qa_collection[-1][1] += line
+
+    # print(qa_collection[:4])
+    p.save_as(array=qa_collection, dest_file_name=f"{filename}.xls")
+
+
+# easy_find(open_txt('guru99'))
+regex_find(open_txt('guru99'))
