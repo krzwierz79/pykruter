899446877bcce5cc904cb5cc331bddcec360fa56
krzwierz79 <krzwierz79@gmail.com>
2022-04-02 16:52:48 +0200

    more scraped questions

--- data/questions/01_web_scrape.py
+++ data/questions/01_web_scrape.py
@@ -1,17 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-URL = "https://data-flair.training/blogs/top-python-interview-questions-answer/"
+URL = "https://hackr.io/blog/python-interview-questions"
 # URL = "https://www.projectpro.io/article/100-data-science-in-python-interview-questions-and-answers-for-2021/188"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-p_tags = soup.find_all("p")
+# p_tags = soup.find_all("p")
 # p_tags = soup.find_all("p", {"style": "text-align: left;"})
+tags = soup.find_all(['h4', 'p'])  # list all needed tags
 for tag in p_tags:
-    tag_c = tag.get_text()
-    print(tag_c)
+    tag_t = tag.get_text()
+    print(tag_t)
 
 # scrape.py >> plik.txt
